b75c7e383cd1b73363ca82b7ddc704a8e1ddc6e6
mztejero <mztejero@gmail.com>
2026-03-04 12:10:33 -0800

    main.cpp Player.cpp Player.h

---
+++ Player.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Player.h"
+
+    // constructors
+    Player::Player() {
+        name = "Hero";
+        health = 100;
+        maxHealth = 100;
+        attackPower = 10;
+    };
+    Player::Player(std::string name, int health, int attackPower) {
+        this->name = name;
+        this->health = health;
+        this->maxHealth = health;
+        this->attackPower = attackPower;
+    };
+    Player::~Player() {
+        std::cout << "The object for Player " << name << " has been destroyed" << std::endl;
+    }
+
+    // getters
+    std::string Player::getName() const {
+        return name;
+    };
+    int Player::getHealth() const {
+        return health;
+    };
+    int Player::getMaxHealth() const {
+        return maxHealth;
+    };
+    int Player::getAttackPower() const {
+        return attackPower;
+    };
+
+    // actual functions
+    void Player::takeDamage(int damage) {
+        if (damage > health) {
+            health = 0;
+        }
+        else {
+            health -= damage;
+        }
+
+        if (health <= 0) {
+            std::cout << name << " falls to the grond, defeated." << std::endl;
+        }
+    };
+    void Player::displayStatus() {
+        std::cout << name << " - HP: " << health << "/" << maxHealth << std::endl;
+    };
---
+++ Player.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player {
+    private:
+        std::string name;
+        int health;
+        int maxHealth;
+        int attackPower;
+
+    public:
+    // constructors
+    Player();
+    Player(std::string name, int health, int attackPower);
+    ~Player();
+
+    // getters
+    std::string getName() const;
+    int getHealth() const;
+    int getMaxHealth() const;
+    int getAttackPower() const;
+
+    // actual functions
+    void takeDamage(int damage);
+    void displayStatus();
+
+};
+
+#endif
